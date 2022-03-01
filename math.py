a20c13a924b831e5fa7b4c0f047ec7c380227589
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-01 02:10:16 +0000

    math program

--- math.py
+++ math.py
@@ -8,22 +8,23 @@
 def main():
     # This function calculates tha area and perimetre of a rectangle
     import math
-    
+
     # input
     length = int(input("Enter the length of the rectangle (mm): "))
     width = int(input("Enter the width of the rectangle (mm): "))
-    
-    #process
+
+    # process
     area = length * width
     perimetre = 2 * (length + width)
-    
-    #output
+
+    # output
     print("")
-    
+
     print("Area is {} cm².".format(area))
     print("Perimetre is {} cm.".format(perimetre))
-    
+
     print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
